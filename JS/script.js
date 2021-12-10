20ead1cc8d58d2f88172788390b649a542304ad8
Liam <liam.mcdougall1999@gmail.com>
2021-12-10 13:17:27 -0800

    attempt at dark mode by replacing what the class of body is

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+document.querySelector('toggle').addEventListener(
+    'click', themeswitch()
+)
+
+function themeswitch(){
+    if(document.body(".light-theme")(
+    document.removeClass('light-theme').addClass('dark-theme')
+    ))
+
+    if(document.body(".dark-theme")(
+        document.removeClass('dark-theme').addClass('light-theme')
+        ))
+
+    )
+}
